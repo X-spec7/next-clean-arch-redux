94dce489d14f027f0208dd43d17972029d865884
X-spec7 <mercury.spec77@gmail.com>
2024-08-21 04:35:21 +0900

    refactor: setup redux

---
+++ src/redux/store.ts
@@ -0,0 +1,26 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit";
+import { combineSlices, configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./features/auth/authSlice";
+
+const rootReducer = combineSlices(authSlice);
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const createStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware().concat();
+    },
+  });
+};
+
+export type AppStore = ReturnType<typeof createStore>;
+
+export type AppDispatch = AppStore["dispatch"];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
